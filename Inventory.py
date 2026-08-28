f73ed6aec8aa32195e9dd3490952c7d80bb1b1d3
Bibek Bhattarai <bkbhattarai4@gmail.com>
2026-08-28 15:31:42 +0200

    Inventory.py

--- Inventory.py
+++ Inventory.py
@@ -8,7 +8,7 @@ st.title("🏭 Laserax GmbH Inventory Manager")
 
 # --- CONFIGURATION: Link to your shared OneDrive Sheet ---
 # Paste the direct download link to the inventory.xlsx hosted in your shared OneDrive here
-ONEDRIVE_FILE_URL = "https://laseraxinc-my.sharepoint.com/:x:/g/personal/bbhattarai_laserax_com/IQDlpeWcuGCsTKPotwyxsN8fAZVN6H-adOr3sQTjHeCWd5w?e=gpybkY"
+ONEDRIVE_FILE_URL = "https://laseraxinc-my.sharepoint.com/:x:/g/personal/bbhattarai_laserax_com/IQDlpeWcuGCsTKPotwyxsN8fAZVN6H-adOr3sQTjHeCWd5w?e=gpybkY&download=1"
 
 # Columns mapped exactly to your Excel image headers
 COLUMNS = ["S.No", "EQUIPMENT", "LASERAX PROJECT No. - Part NO", "STOCK", "LOCATION", "REMARKS", "PROCUREMENT LINK"]
