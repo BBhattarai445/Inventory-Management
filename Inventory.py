370d10ddf85b2e5fd5b042dd316a2677fed2186a
Bibek Bhattarai <bkbhattarai4@gmail.com>
2026-08-31 13:51:06 +0200

    Update Inventory.py

--- Inventory.py
+++ Inventory.py
@@ -12,14 +12,14 @@ st.title("🏭 Laserax GitHub Excel Inventory Manager (Online Cloud Portal)")
 # --- CONFIGURATION: GitHub Repository Details ---
 # ==============================================================================
 # Ensure these are accurate and your file inside GitHub is named exactly 'Inventory.xlsx'
-GITHUB_USER = "/BBhattarai445"
-GITHUB_REPO = "Inventory-Management"
-FILE_PATH = "Inventory.xlsx"  
+GITHUB_USER = r"/BBhattarai445"
+GITHUB_REPO = r"Inventory-Management"
+FILE_PATH = r"Inventory.xlsx"  
 
 # Secure connection using your Streamlit secret token
 GITHUB_TOKEN = st.secrets.get("GITHUB_TOKEN", "")
 API_URL = f"https://github.com{GITHUB_USER}/{GITHUB_REPO}/contents/{FILE_PATH}"
-RAW_URL = f"https://githubusercontent.com{GITHUB_USER}/{GITHUB_REPO}/blob/{FILE_PATH}"
+RAW_URL = f"https://githubusercontent.com{GITHUB_USER}/{GITHUB_REPO}/blob/main/{FILE_PATH}"
 
 def save_to_github(dataframe):
     """
